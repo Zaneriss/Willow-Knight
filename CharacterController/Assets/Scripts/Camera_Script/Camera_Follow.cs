05e9cbccc7a0cd0b4956f05147b9a803f4cd5bdc
Zaneriss <48700461+Zaneriss@users.noreply.github.com>
2019-03-19 20:56:47 -0400

    Willow Knight

---
+++ CharacterController/Assets/Scripts/Camera_Script/Camera_Follow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour
+{
+
+    public Transform target;
+
+    public float smoothSpeed;
+    public Vector3 offset;
+
+    void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        
+    }
+
+}
