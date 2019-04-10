e1bd262ad6f5eb5fae53fc428d6238bb297fd853
Dex10k <ddeboone@gmail.com>
2019-04-09 21:49:31 -0400

    Burden Initial Commit
    Burden Script Start

---
+++ CharacterController/Assets/WillowKnight/Gameplay/Scripts/Classes/NPC_Scripts/TheBurden.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBurden : EnemyBaseScript
+{
+
+    [Range(1, 17)]
+    public float FallSpeedInUnitsPerSecond = 1;
+
+
+
+    public override void Movement(){
+
+    }
+
+    // Update is called once per frame
+    protected override void SelfDestruct(){
+
+    }
+}
